a69968777984ed63d948bf7ba7c75d1d8c5698bd
Nikita <savvin.nikit@yandex.ru>
2025-11-21 10:46:35 +0300

    feat: change create chats

--- src/api/api_routers.py
+++ src/api/api_routers.py
@@ -9,8 +9,8 @@ api_router.include_router(chat, prefix="/chat", tags=["Chat"])
 from src.api.v1.chat_history import app as chat_history
 api_router.include_router(chat_history, prefix="/chat_history", tags=["Chat History"])
 
-from src.api.v1.create_chat import app as create_chat
-api_router.include_router(create_chat, prefix="/create_new_chat", tags=["Create Chat"])
+# from src.api.v1.create_chat import app as create_chat
+# api_router.include_router(create_chat, prefix="/create_new_chat", tags=["Create Chat"])
 
 from src.api.v1.delete_chat import app as delete_chat
 api_router.include_router(delete_chat, prefix="/delete_chat", tags=["Delete Chat"])
