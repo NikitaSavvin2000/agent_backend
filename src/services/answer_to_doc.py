85df5c92d0d9c1ab1a2a8b98e9912c841eb12a28
Nikita <savvin.nikit@yandex.ru>
2026-03-05 13:41:33 +0300

    fix: chrome

--- src/services/answer_to_doc.py
+++ src/services/answer_to_doc.py
@@ -132,8 +132,13 @@ def html_to_png_blocks(html_code: str, block_selector: str = "div", scale: float
         html_path = f.name
 
     chrome_options = Options()
+    # chrome_options.add_argument("--headless=new")
+    # chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--window-size=1920,1080")
     chrome_options.add_argument(f"--force-device-scale-factor={scale}")
 
     driver = webdriver.Chrome(options=chrome_options)
